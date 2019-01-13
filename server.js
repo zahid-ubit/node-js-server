628b2d92c4d728a392dd6d895389e6ebfc963c1d
unknown <zahid-ubit>
2019-01-13 19:15:14 +0500

    setup heroku update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express=require('express');
 const hbs=require('hbs'); 
-const port =process.env.port;
+const port =process.env.PORT||3000;
 var app=express();
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname +'/views/partials');
